84bd431d9e88fb580c32c1a01e98b5c4285ae837
Neth Horn <nethwebdev@gmail.com>
2018-10-10 14:10:58 -0700

    Array Binary Search is working.

---
+++ challenges/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int CheckArrayForValue(int[] arr, int val)
+        {
+            int mid;
+            int start = 0;
+            int end = arr.Length - 1;
+            while (start <= end)
+            {
+                mid = (start + end) / 2;
+                if (arr[mid] == val)
+                {
+                    return mid;
+                }
+                else if (arr[mid] < val)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
