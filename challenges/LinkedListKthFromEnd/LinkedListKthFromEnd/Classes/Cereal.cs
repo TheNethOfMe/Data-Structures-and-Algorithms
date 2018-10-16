c162800756db0257cd88d122d478da1c92e188fb
Neth Horn <nethwebdev@gmail.com>
2018-10-16 13:55:17 -0700

    Initial commit for this branch. Setup my node (Cereal) and list (CerealList) classes.

---
+++ challenges/LinkedListKthFromEnd/LinkedListKthFromEnd/Classes/Cereal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListKthFromEnd.Classes
+{
+    public class Cereal
+    {
+        public string Value { get; set; }
+        public Cereal Next { get; set; }
+
+        public Cereal(string value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ challenges/LinkedListKthFromEnd/LinkedListKthFromEnd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LinkedListKthFromEnd.Classes;
+
+namespace LinkedListKthFromEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static CerealList CreateInitialList()
+        {
+            Cereal LuckyCharms = new Cereal("Lucky Charms");
+            CerealList StarterList = new CerealList(LuckyCharms);
+            Cereal AppleJacks = new Cereal("Apple Jacks");
+            Cereal Trix = new Cereal("Trix");
+            Cereal FruitLoops = new Cereal("Fruit Loops");
+            Cereal CocoaPebbles = new Cereal("Cocoa Pebbles");
+            StarterList.AddCereal(AppleJacks);
+            StarterList.AppendCereal(Trix);
+            StarterList.AddBefore(FruitLoops, "Lucky Charms");
+            StarterList.AddAfter(CocoaPebbles, "Lucky Charms");
+            return StarterList;
+        }
+    }
+}
