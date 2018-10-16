4782b62256b43b15f4b6f992bb49930a45407a3c
Neth Horn <nethwebdev@gmail.com>
2018-10-16 14:09:36 -0700

    Recreated the challenge with proper setup

--- challenges/LinkedListKthFromEnd/LinkedListKthFromEnd/Program.cs
+++ challenges/LinkedListKthFromEnd/LinkedListKthFromEnd/Program.cs
@@ -1,28 +1,15 @@
 ﻿using System;
-using LinkedListKthFromEnd.Classes;
+using linkedList;
+using linkedList.Classes;
 
-namespace LinkedListKthFromEnd
+namespace linkedListKthFromEnd
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        }
-
-        public static CerealList CreateInitialList()
-        {
-            Cereal LuckyCharms = new Cereal("Lucky Charms");
-            CerealList StarterList = new CerealList(LuckyCharms);
-            Cereal AppleJacks = new Cereal("Apple Jacks");
-            Cereal Trix = new Cereal("Trix");
-            Cereal FruitLoops = new Cereal("Fruit Loops");
-            Cereal CocoaPebbles = new Cereal("Cocoa Pebbles");
-            StarterList.AddCereal(AppleJacks);
-            StarterList.AppendCereal(Trix);
-            StarterList.AddBefore(FruitLoops, "Lucky Charms");
-            StarterList.AddAfter(CocoaPebbles, "Lucky Charms");
-            return StarterList;
+            
         }
     }
 }
