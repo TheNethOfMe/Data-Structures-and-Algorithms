def8412eeef511329167c266e46facc67b7d329d
Neth Horn <nethwebdev@gmail.com>
2018-10-17 21:05:41 -0700

    Created passing test suite

--- challenges/LinkedListMerge/LinkedListMerge/Program.cs
+++ challenges/LinkedListMerge/LinkedListMerge/Program.cs
@@ -4,32 +4,12 @@ using linkedList.Classes;
 
 namespace LinkedListMerge
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            string[] cerealList1 = new string[4]
-            {
-                "Lucky Charms",
-                "Cocoa Pops",
-                "Trix",
-                "Boo Berry"
-            };
-            string[] cerealList2 = new string[4]
-            {
-                "Corn Pops",
-                "Smacks",
-                "Special K",
-                "Frosted Flakes"
-            };
-            string[] cerealList3 = new string[2]
-            {
-                "Cocoa Pebbles",
-                "Fruity Pebbles"
-            };
-            CerealList GeneralMills = generateList(cerealList1);
-            CerealList Kellogs = generateList(cerealList2);
+            Console.WriteLine("Hello World!");CerealList GeneralMills = genListOne();
+            CerealList Kellogs = genListTwo();
             Console.WriteLine("General Mills");
             GeneralMills.PrintList();
             Console.WriteLine("Kellogs");
@@ -40,7 +20,7 @@ namespace LinkedListMerge
             Console.ReadKey();
         }
 
-        static CerealList Merge(CerealList listA, CerealList listB)
+        public static CerealList Merge(CerealList listA, CerealList listB)
         {
             listA.Current = listA.Head;
             listB.Current = listB.Head;
@@ -64,7 +44,7 @@ namespace LinkedListMerge
             return listA;
         }
 
-        static CerealList generateList(string[] cerealList)
+        public static CerealList generateList(string[] cerealList)
         {
             CerealList temp = new CerealList(new Cereal(cerealList[0]));
             for (int i = 1; i < cerealList.Length; i++)
@@ -72,6 +52,40 @@ namespace LinkedListMerge
                 temp.AddCereal(new Cereal(cerealList[i]));
             }
             return temp;
+        }
+
+        public static CerealList genListOne()
+        {
+            string[] cerealList1 = new string[4]
+            {
+                "Lucky Charms",
+                "Cocoa Puffs",
+                "Trix",
+                "Boo Berry"
+            };
+            return generateList(cerealList1);
+        }
+
+        public static CerealList genListTwo()
+        {
+            string[] cerealList2 = new string[4]
+            {
+                "Corn Pops",
+                "Smacks",
+                "Special K",
+                "Frosted Flakes"
+            };
+            return generateList(cerealList2);
+        }
+
+        public static CerealList genListThree()
+        {
+            string[] cerealList3 = new string[2]
+            {
+                "Cocoa Pebbles",
+                "Fruity Pebbles"
+            };
+            return generateList(cerealList3);
         }
     }
 }
--- challenges/LinkedListMerge/MergeTests/UnitTest1.cs
+++ challenges/LinkedListMerge/MergeTests/UnitTest1.cs
@@ -1,14 +1,60 @@
 using System;
 using Xunit;
+using LinkedListMerge;
+using linkedList.Classes;
 
 namespace MergeTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(0, "Boo Berry")]
+        [InlineData(1, "Frosted Flakes")]
+        [InlineData(2, "Trix")]
+        [InlineData(3, "Special K")]
+        [InlineData(4, "Cocoa Puffs")]
+        [InlineData(5, "Smacks")]
+        [InlineData(6, "Lucky Charms")]
+        [InlineData(7, "Corn Pops")]
+        public void ShouldMergeEqualLengthLists(int index, string expectedOutput)
         {
+            CerealList GeneralMills = Program.genListOne();
+            CerealList Kellogs = Program.genListTwo();
+            CerealList CombinedList = Program.Merge(GeneralMills, Kellogs);
+            string[] CombinedArr = CombinedList.ListToArr(8);
+            Assert.Equal(expectedOutput, CombinedArr[index]);
+        }
 
+        [Theory]
+        [InlineData(0, "Boo Berry")]
+        [InlineData(1, "Fruity Pebbles")]
+        [InlineData(2, "Trix")]
+        [InlineData(3, "Cocoa Pebbles")]
+        [InlineData(4, "Cocoa Puffs")]
+        [InlineData(5, "Lucky Charms")]
+        public void ShouldMergeWhenListAIsLonger(int index, string expectedOutput)
+        {
+            CerealList GeneralMills = Program.genListOne();
+            CerealList Post = Program.genListThree();
+            CerealList CombinedList = Program.Merge(GeneralMills, Post);
+            string[] CombinedArr = CombinedList.ListToArr(6);
+            Assert.Equal(expectedOutput, CombinedArr[index]);
+        }
+
+        [Theory]
+        [InlineData(0, "Fruity Pebbles")]
+        [InlineData(1, "Frosted Flakes")]
+        [InlineData(2, "Cocoa Pebbles")]
+        [InlineData(3, "Special K")]
+        [InlineData(4, "Smacks")]
+        [InlineData(5, "Corn Pops")]
+        public void ShouldMergeWhenListBIsLonger(int index, string expectedOutput)
+        {
+            CerealList Post = Program.genListThree();
+            CerealList Kellogs = Program.genListTwo();
+            CerealList CombinedList = Program.Merge(Post, Kellogs);
+            string[] CombinedArr = CombinedList.ListToArr(6);
+            Assert.Equal(expectedOutput, CombinedArr[index]);
         }
     }
 }
