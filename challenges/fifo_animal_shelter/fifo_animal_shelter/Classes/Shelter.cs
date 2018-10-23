dd5aff409c74ff32e63dc7e4a396d54e6d7b4d1e
Neth Horn <nethwebdev@gmail.com>
2018-10-23 15:16:21 -0700

    created classes, methods, and passing test suite

---
+++ challenges/fifo_animal_shelter/fifo_animal_shelter/Classes/Shelter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fifo_animal_shelter.Classes
+{
+    public class Shelter
+    {
+        public Pet Front { get; set; }
+        public Pet Rear { get; set; }
+
+        /// <summary>
+        /// Creates a new Shelter with one pet that will be the front and rear until more are added
+        /// </summary>
+        /// <param name="pet">The node that will start the queue</param>
+        public Shelter(Pet pet)
+        {
+            Front = pet;
+            Rear = pet;
+        }
+
+        /// <summary>
+        /// Returns the Pet in front of the Shelter without altering it
+        /// </summary>
+        /// <returns>The Pet in front</returns>
+        public Pet Peek()
+        {
+            return Front;
+        }
+
+        /// <summary>
+        /// Adds a node to the rear of the queue
+        /// </summary>
+        /// <param name="pet">the node that will be added to the queue</param>
+        public void Enqueue(Pet node)
+        {
+            Rear.Next = node;
+            Rear = node;
+        }
+
+        /// <summary>
+        /// Takes the first pet of the prefered type out of the shelter
+        /// </summary>
+        /// <param name="prefered">A Pet of the type that will be returned</param>
+        /// <returns>A Pet depending on the type of prefered Pet passed in</returns>
+        public Pet Dequeue(Pet preferred)
+        {
+            Pet result = null;
+            Pet temp = null;
+            if (preferred.GetType() == Front.GetType())
+            {
+                result = Dequeue();
+            }
+            else
+            {
+                temp = Dequeue();
+                Enqueue(temp);
+                while (Front != temp)
+                {
+                    if ((Front.GetType() == preferred.GetType()) && (result == null))
+                    {
+                        result = Dequeue();
+                    }
+                    else
+                    {
+                        Enqueue(Dequeue());
+                    }
+                }
+            }
+            if (result == null)
+            {
+                result = Dequeue();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Takes the pet in front out of the shelter, because he/she found a home!
+        /// </summary>
+        /// <returns>The Pet that is going to his/her new home</returns>
+        public Pet Dequeue()
+        {
+            Pet temp = Peek();
+            Front = Front.Next;
+            temp.Next = null;
+            return temp;
+        }
+    }
+}
---
+++ challenges/fifo_animal_shelter/fifo_animal_shelter/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using fifo_animal_shelter.Classes;
+
+namespace fifo_animal_shelter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Creates a shelter with five animals for the purposes of testing
+        /// </summary>
+        /// <returns>A shelter with five Pet nodes inside</returns>
+        public static Shelter PopulateShelter()
+        {
+            Dog Jason = new Dog("Jason");
+            Dog Kim = new Dog("Kim");
+            Cat Zach = new Cat("Zach");
+            Cat Trini = new Cat("Trini");
+            Dog Billy = new Dog("Billy");
+            Shelter result = new Shelter(Jason);
+            result.Enqueue(Kim);
+            result.Enqueue(Zach);
+            result.Enqueue(Trini);
+            result.Enqueue(Billy);
+            return result;
+        }
+
+        /// <summary>
+        /// Creates a Shelter that only contains Cat nodes again for testing purposes
+        /// </summary>
+        /// <returns>A Shelter with three Cat nodes</returns>
+        public static Shelter CatsOnly()
+        {
+            Cat Rocky = new Cat("Rocky");
+            Cat Aiesha = new Cat("Aiesha");
+            Cat Adam = new Cat("Adam");
+            Shelter result = new Shelter(Rocky);
+            result.Enqueue(Aiesha);
+            result.Enqueue(Adam);
+            return result;
+        }
+    }
+}
