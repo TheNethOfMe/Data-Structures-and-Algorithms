dd54646451cf2669cab6d9ff6789dc31af1ebd97
Neth Horn <nethwebdev@gmail.com>
2018-10-26 17:41:08 -0700

    Wrote Diminishing (Einie meanie miney moe) method. Seems to be working.

---
+++ challenges/diminishing_lists/diminishing_lists/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace diminishing_lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<string> optionList = GenerateList();
+            PrintList(optionList);
+            string result = Diminish(optionList, 2);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        static string Diminish(List<string> strList, int num)
+        {
+            Console.WriteLine("Diminishing");
+            int pointer = num - 1;
+            while (strList.Count > 1)
+            {
+                strList.RemoveAt(pointer);
+                pointer = pointer + num;
+                if (pointer >= strList.Count)
+                {
+                    pointer = pointer - strList.Count;
+                }
+                Console.WriteLine("++++++++");
+                PrintList(strList);
+            }
+            return strList[0];
+        }
+
+        static List<string> GenerateList()
+        {
+            List<string> result = new List<string>
+            {
+                "test one",
+                "test two",
+                "test three",
+                "test four",
+                "test five",
+                "test six"
+            };
+            return result;
+        }
+
+        static void PrintList(List<string> strList)
+        {
+            foreach (string str in strList)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
