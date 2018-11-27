3682c274d181df89ebdaa0fefae77b3cab601599
Neth Horn <nethwebdev@gmail.com>
2018-11-27 15:26:02 -0800

    Methods are set. Demo works.

---
+++ data-structures/graphs/TestGraph/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using graphs.Classes;
+
+namespace TestGraph
+{
+    public class UnitTest1
+    {
+        [Fact]
+        // Should create a Graph
+        public void ShouldCreateAGraph()
+        {
+
+        }
+    }
+}
